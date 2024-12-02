bc25559e7a208aeef522dd75ba627affab196c38
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2024-12-02 18:51:47 +0200

    imperative code

---
+++ Database.fs
@@ -0,0 +1,11 @@
+﻿module Database
+
+open System
+open MySql.Data.MySqlClient
+
+let connectionString = "Server=localhost;Database=stock_management;User Id=root;Password=;"
+
+let connect() =
+  let connection = new MySqlConnection(connectionString)
+  connection.Open()
+  connection
---
+++ Reports.fs
@@ -0,0 +1,27 @@
+﻿module Reports
+
+open System
+open Database
+open MySql.Data.MySqlClient
+
+let totalSalesReport () =
+    use connection = Database.connect()
+    let query = "SELECT SUM(TotalCost) AS TotalSales FROM `Order`"
+    use command = new MySqlCommand(query, connection)
+    use reader = command.ExecuteReader()
+    if reader.Read() then
+        let totalSales = reader.GetDecimal(0)
+        printfn "Total Sales: %.2f" totalSales
+    else
+        printfn "No sales found."
+
+let inventoryValueReport () =
+    use connection = Database.connect()
+    let query = "SELECT SUM(Price * Quantity) AS InventoryValue FROM Product"
+    use command = new MySqlCommand(query, connection)
+    use reader = command.ExecuteReader()
+    if reader.Read() then
+        let inventoryValue = reader.GetDecimal(0)
+        printfn "Total Inventory Value: %.2f" inventoryValue
+    else
+        printfn "No products found."
